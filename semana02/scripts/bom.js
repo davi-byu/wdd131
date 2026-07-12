47c84225c04614fad664cbcf7da44b9a0849d7a4
davi-byu <davivictor2016.2@gmail.com>
2026-07-12 07:18:56 -0300

    finalizado

--- semana02/scripts/bom.js
+++ semana02/scripts/bom.js
@@ -2,17 +2,34 @@ const input = document.querySelector('#capfav');
 const botao = document.querySelector('button');
 const lista = document.querySelector('#list');
 
+botao.addEventListener('click', function () {
+    
+    if (input.value.trim() === '') {
+        alert('Digite um livro e capítulo.');
+        input.focus();
+        return;
+    }
+
 const li = document.createElement('li');
 const botaoExcluir = document.createElement('button');
 
-li.textContent = input.value;
+li.textContent = input.value.trim();
 
 botaoExcluir.textContent = '❌';
 
 botaoExcluir.setAttribute(
     'aria-label',
-    `Excluir ${input.value}`
-);
+    `Excluir ${li.textContent}`
+    );
 
 li.append(botaoExcluir);
-lista.append(li);
+    lista.append(li);
+
+    input.value = '';
+    input.focus();
+
+    botaoExcluir.addEventListener('click', function () {
+        lista.removeChild(li);
+        input.focus();
+});
+});

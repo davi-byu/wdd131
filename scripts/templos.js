f1face6eccbb3578cd93de900e01a3197561d745
davi-byu <davivictor2016.2@gmail.com>
2026-07-14 21:33:56 -0300

    ....

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,7 +1,7 @@
 const menuButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-menuButton.addEventListener('click', () => {
+menuButton.addEventListener('click', function () {
     navigation.classList.toggle('open');
     menuButton.classList.toggle('open');
 
